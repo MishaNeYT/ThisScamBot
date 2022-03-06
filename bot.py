d7cbbdb507e7d85d350c9e744cd38f03b419be54
Mxnov <93668921+Mcinv@users.noreply.github.com>
2022-03-06 10:40:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import pyautogui, time
 time.sleep(5); f open("тут должен быть указан файл с готовыми сообщениями, например, aboba.txt (и в этой aboba.txt есть нужные сообщения для спама)", "r")
-for line in f:
-                  
+for line in f:        
     pyautogui.typewrite(line)
     pyautogui.press("enter")
